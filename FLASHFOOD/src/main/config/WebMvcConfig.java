14d43feb00d39081d228a38029a93a0be0a68d17
Jayden Wang <jaydenwang@umass.edu>
2023-05-07 22:17:39 -0400

    update mybaties

--- FLASHFOOD/src/main/config/WebMvcConfig.java
+++ FLASHFOOD/src/main/config/WebMvcConfig.java
@@ -29,6 +29,23 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
         log.info("Finsihed mapping static resources [used time" + (System.currentTimeMillis() - l) + "ms]");
     }
+    /**
+     * exted message MVC converters
+     * <p>Since the original built-in message converter cannot handle the problem of front-end js data loss, for example, the 19-digit id will be converted to 18-digit, so a custom message converter is required. This converter has been defined in the common JacksonConfig up,
+     * You can convert long type data into String type data and send it to the front end, so that the front end will not lose data.
+     *
+     * @param converters list
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // Create converter object
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        // change object to Json
+        converter.setObjectMapper(new JacksonObjectMapper());
+        // add customoer converter
+        converters.add(0, converter); // 0 meas first postion of converter
 
+        log.info("Customer converter is added.");
+    }
 
 }
