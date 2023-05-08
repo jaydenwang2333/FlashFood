9296b65c7dd568778393bd56a5a050ff2c7d0641
Jayden Wang <jaydenwang@umass.edu>
2023-05-08 00:13:38 -0400

    common 1st

---
+++ FLASHFOOD/src/main/common/BaseContext.java
@@ -0,0 +1,13 @@
+
+package config;
+public class BaseContext {
+    private static final ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    public static void setCurrentId(Long id) {
+        threadLocal.set(id);
+    }
+
+    public static long getCurrentId() {
+        return threadLocal.get();
+    }
+}
