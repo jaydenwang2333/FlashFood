b1ff115fe3e9c15a2b70294218c339622db48d67
XiaoUMASS <xwang6@umass.edu>
2023-05-08 20:00:28 -0400

    Added README & Created service file

---
+++ FLASHFOOD/src/main/service/AddressBookService.java
@@ -0,0 +1,19 @@
+package service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+public interface AddressBookService extends IService<AddressBook> {
+    void saveAddress(AddressBook addressBook);
+    List<AddressBook> queryAddressBookByUserId(Long userId);
+    void setDefaultAddress(Long id);
+    AddressBook getAddressBookById(Long id);
+    void updateAddressBook(AddressBook addressBook);
+    AddressBook getDefaultAddressBook();
+    void deleteAddressById(Long ids);
+    AddressBook lastUpdateAddress();
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# FlashFood
+## overall explanation
+### This is a course project built by the following group members:
+### Xiao Wang
+### Wei Li
+### Xinpeng Liu
+### Jayden Wang
+
