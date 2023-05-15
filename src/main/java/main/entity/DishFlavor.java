e81358dcadb9906e3dd73321bb86623b779cbdff
Weiqiang Li <lwq617597704@gmail.com>
2023-05-15 00:57:53 -0400

    Dish flavor class

---
+++ src/main/java/main/entity/DishFlavor.java
@@ -0,0 +1,48 @@
+package main.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Flavor
+ */
+@Data
+public class DishFlavor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+
+    //dish id
+    private Long dishId;
+
+
+    //Flavor
+    private String name;
+
+    private String value;
+
+
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+
+    private Integer isDeleted;
+
+}
